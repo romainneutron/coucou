89625ac365072bee264c68e2d4ccb5f2792b1195
Romain Neutron <imprec@gmail.com>
2018-02-20 13:26:13 +0100

    Json

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController
 {
     public function index(Request $request)
     {
-        usleep(max(0, min(5000000, $request->query->get('usleep', 0))));
+        $duration = max(0, min(5000000, $request->query->get('usleep', 0)));
 
-        return new Response('Coucou');
+        if ($duration) {
+            usleep($duration);
+        }
+
+        return new JsonResponse([
+            'usleeped' =>  $duration,
+            'message' => 'coucou',
+        ]);
     }
 }
