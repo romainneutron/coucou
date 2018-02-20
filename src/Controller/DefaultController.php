1cc5a90a6dd494a854120d6f0123c01e7c5e316c
Romain Neutron <imprec@gmail.com>
2018-02-20 13:09:49 +0100

    Add sleep

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController
 {
-    public function index()
+    public function index(Request $request)
     {
+        usleep(min(0, max(5000000, $request->query->get('usleep', 0))));
+
         return new Response('Coucou');
     }
 }
